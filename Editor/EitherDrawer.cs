e228af811c2e5309feb657b8915ec8ff86f5ca69
bgsulz <bgs.mail1234@gmail.com>
2022-03-21 17:12:34 -0400

    Added namespace

--- Editor/EitherDrawer.cs
+++ Editor/EitherDrawer.cs
@@ -5,100 +5,104 @@ using System.Linq;
 using UnityEngine;
 using UnityEditor;
 
-[CustomPropertyDrawer(typeof(Either<,>),   true),
- CustomPropertyDrawer(typeof(Either<,,>),  true),
- CustomPropertyDrawer(typeof(Either<,,,>), true)]
-public class EitherDrawer : PropertyDrawer
+namespace Extra.Either
 {
-    private static readonly float ButtonWidth = EditorGUIUtility.singleLineHeight;
-    private static string AsTypePropertyName(int index) => $"<AsT{index}>k__BackingField";
-
-    private string[] _popupOptions;
-    private GUIStyle _popupStyle;
-    private float _propertyHeight;
-
-    // Maps GetPropertyHeight to a variable so it can change depending on selected type.
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => _propertyHeight;
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(Either<,>), true),
+     CustomPropertyDrawer(typeof(Either<,,>), true),
+     CustomPropertyDrawer(typeof(Either<,,,>), true)]
+    public class EitherDrawer : PropertyDrawer
     {
-        // Retrieve property value as type Either using reflection.
-        if (fieldInfo.GetValue(property.serializedObject.targetObject) is not Either propertyValue)
+        private static readonly float ButtonWidth = EditorGUIUtility.singleLineHeight;
+        private static string AsTypePropertyName(int index) => $"<AsT{index}>k__BackingField";
+
+        private string[] _popupOptions;
+        private GUIStyle _popupStyle;
+        private float _propertyHeight;
+
+        // Maps GetPropertyHeight to a variable so it can change depending on selected type.
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => _propertyHeight;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            throw new Exception($"Property {property.name} is not of type {nameof(Either)}.");
-        }
-
-        // If not yet initialized, initialize type selection popup style and type options list.
-        InitializePopupStyles(propertyValue, ref _popupStyle, ref _popupOptions);
-
-        // Gather all properties representing the differently typed values of the object.
-        var typeCount = propertyValue.TypeCount;
-        var props = GatherSerializedProperties(property, typeCount);
-
-        // Retrieve index property, which keeps track of selected type.
-        var index = property.FindPropertyRelative("index");
-
-        label = EditorGUI.BeginProperty(position, label, property);
-        {
-            EditorGUI.BeginChangeCheck();
-
-            // Segment position Rect into left and right parts for button and property field respectively.
-            var (buttonRect, propertyRect) = CalculateButtonAndPropertyRects(position, label);
-
-            // Draw popup and use recorded index value to determine selected type.
-            var newIndex = EditorGUI.Popup(buttonRect, index.intValue, _popupOptions, _popupStyle);
-            index.intValue = newIndex;
-            var propertyToDraw = props[newIndex];
-
-            // Draw appropriate property and set height.
-            if (propertyToDraw == null)
+            // Retrieve property value as type Either using reflection.
+            if (fieldInfo.GetValue(property.serializedObject.targetObject) is not Either propertyValue)
             {
-                EditorGUI.LabelField(propertyRect, "Unable to serialize this property.");
-                _propertyHeight = EditorGUIUtility.singleLineHeight;
+                throw new Exception($"Property {property.name} is not of type {nameof(Either)}.");
             }
-            else
+
+            // If not yet initialized, initialize type selection popup style and type options list.
+            InitializePopupStyles(propertyValue, ref _popupStyle, ref _popupOptions);
+
+            // Gather all properties representing the differently typed values of the object.
+            var typeCount = propertyValue.TypeCount;
+            var props = GatherSerializedProperties(property, typeCount);
+
+            // Retrieve index property, which keeps track of selected type.
+            var index = property.FindPropertyRelative("index");
+
+            label = EditorGUI.BeginProperty(position, label, property);
             {
-                EditorGUI.PropertyField(propertyRect, propertyToDraw, GUIContent.none);
-                _propertyHeight = EditorGUI.GetPropertyHeight(propertyToDraw, label, true);
+                EditorGUI.BeginChangeCheck();
+
+                // Segment position Rect into left and right parts for button and property field respectively.
+                var (buttonRect, propertyRect) = CalculateButtonAndPropertyRects(position, label);
+
+                // Draw popup and use recorded index value to determine selected type.
+                var newIndex = EditorGUI.Popup(buttonRect, index.intValue, _popupOptions, _popupStyle);
+                index.intValue = newIndex;
+                var propertyToDraw = props[newIndex];
+
+                // Draw appropriate property and set height.
+                if (propertyToDraw == null)
+                {
+                    EditorGUI.LabelField(propertyRect, "Unable to serialize this property.");
+                    _propertyHeight = EditorGUIUtility.singleLineHeight;
+                }
+                else
+                {
+                    EditorGUI.PropertyField(propertyRect, propertyToDraw, GUIContent.none);
+                    _propertyHeight = EditorGUI.GetPropertyHeight(propertyToDraw, label, true);
+                }
             }
+            EditorGUI.EndProperty();
         }
-        EditorGUI.EndProperty();
-    }
 
-    private void InitializePopupStyles(Either propertyValue, ref GUIStyle popupStyle, ref string[] popupOptions)
-    {
-        // Retrieve "three-dots" menu style.
-        popupStyle ??= new GUIStyle(GUI.skin.GetStyle("PaneOptions"))
+        private void InitializePopupStyles(Either propertyValue, ref GUIStyle popupStyle, ref string[] popupOptions)
         {
-            imagePosition = ImagePosition.ImageOnly
-        };
+            // Retrieve "three-dots" menu style.
+            popupStyle ??= new GUIStyle(GUI.skin.GetStyle("PaneOptions"))
+            {
+                imagePosition = ImagePosition.ImageOnly
+            };
 
-        // Transform type Either's "TypeNames" property by prepending each entry with "As ".
-        popupOptions ??= propertyValue.TypeNames.Select(i => $"As {i}").ToArray();
-    }
-
-    private static SerializedProperty[] GatherSerializedProperties(SerializedProperty property, int typeCount)
-    {
-        var res = new SerializedProperty[typeCount];
-        for (var i = 0; i < typeCount; i++)
-        {
-            res[i] = property.FindPropertyRelative(AsTypePropertyName(i));
+            // Transform type Either's "TypeNames" property by prepending each entry with "As ".
+            popupOptions ??= propertyValue.TypeNames.Select(i => $"As {i}").ToArray();
         }
-        return res;
-    }
 
-    private static (Rect, Rect) CalculateButtonAndPropertyRects(Rect position, GUIContent label)
-    {
-        // Determine rect where field will be drawn.
-        var positionNoLabel = EditorGUI.PrefixLabel(position, label);
+        private static SerializedProperty[] GatherSerializedProperties(SerializedProperty property, int typeCount)
+        {
+            var res = new SerializedProperty[typeCount];
+            for (var i = 0; i < typeCount; i++)
+            {
+                res[i] = property.FindPropertyRelative(AsTypePropertyName(i));
+            }
 
-        var buttonRect = positionNoLabel;
-        buttonRect.xMax = buttonRect.xMin + ButtonWidth;
+            return res;
+        }
 
-        var propertyRect = positionNoLabel;
-        propertyRect.xMin += ButtonWidth;
+        private static (Rect, Rect) CalculateButtonAndPropertyRects(Rect position, GUIContent label)
+        {
+            // Determine rect where field will be drawn.
+            var positionNoLabel = EditorGUI.PrefixLabel(position, label);
 
-        return (buttonRect, propertyRect);
+            var buttonRect = positionNoLabel;
+            buttonRect.xMax = buttonRect.xMin + ButtonWidth;
+
+            var propertyRect = positionNoLabel;
+            propertyRect.xMin += ButtonWidth;
+
+            return (buttonRect, propertyRect);
+        }
     }
 }
 
--- Runtime/Either.cs
+++ Runtime/Either.cs
@@ -1,266 +1,271 @@
 ﻿using System;
 using UnityEngine;
 
-/// <summary>
-/// A Serializable, Inspector-friendly class that stores values of multiple types.
-/// </summary>
-[Serializable]
-public abstract class Either
+namespace Extra.Either
 {
     /// <summary>
-    /// Determines selected type.
+    /// A Serializable, Inspector-friendly class that stores values of multiple types.
     /// </summary>
-    [SerializeField] private int index;
-
-    /// <summary>
-    /// Determines selected type. Automatically clamps from 0 to TypeCount.
-    /// </summary>
-    public int Index
+    [Serializable]
+    public abstract class Either
     {
-        get => Mathf.Clamp(index, 0, TypeCount);
-        set => index = value;
+        /// <summary>
+        /// Determines selected type.
+        /// </summary>
+        [SerializeField] private int index;
+
+        /// <summary>
+        /// Determines selected type. Automatically clamps from 0 to TypeCount.
+        /// </summary>
+        public int Index
+        {
+            get => Mathf.Clamp(index, 0, TypeCount);
+            set => index = value;
+        }
+
+        /// <summary>
+        /// How many types this object stores.
+        /// </summary>
+        public abstract int TypeCount { get; }
+
+        /// <summary>
+        /// The name of each type that this object stores, for use in this object's property drawer.
+        /// </summary>
+        public abstract string[] TypeNames { get; }
+
+        /// <summary>
+        /// Retrieves a stored value by type.
+        /// e.g. calling As&#60;float&#62; on an Either&#60;int, float&#62; will return its float value.
+        /// </summary>
+        /// <typeparam name="T">The type to locate.</typeparam>
+        /// <returns>The first stored value of the given type.</returns>
+        public abstract T As<T>();
+
+        /// <summary>
+        /// A helper function for pattern patching.
+        /// </summary>
+        /// <returns>The input parameter as type T.</returns>
+        public static T Self<T0, T>(T0 input) where T0 : T => input;
     }
 
-    /// <summary>
-    /// How many types this object stores.
-    /// </summary>
-    public abstract int TypeCount { get; }
-
-    /// <summary>
-    /// The name of each type that this object stores, for use in this object's property drawer.
-    /// </summary>
-    public abstract string[] TypeNames { get; }
-
-    /// <summary>
-    /// Retrieves a stored value by type.
-    /// e.g. calling As&#60;float&#62; on an Either&#60;int, float&#62; will return its float value.
-    /// </summary>
-    /// <typeparam name="T">The type to locate.</typeparam>
-    /// <returns>The first stored value of the given type.</returns>
-    public abstract T As<T>();
-
-    /// <summary>
-    /// A helper function for pattern patching.
-    /// </summary>
-    /// <returns>The input parameter as type T.</returns>
-    public static T Self<T0, T>(T0 input) where T0 : T => input;
-}
-
-[Serializable]
-public class Either<T0, T1> : Either
-{
-    public override int TypeCount => 2;
-    public override string[] TypeNames => new[] { typeof(T0).Name, typeof(T1).Name };
-
-    /// <summary>
-    /// The stored value of the first type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T0 AsT0 { get; private set; }
-
-    /// <summary>
-    /// The stored value of the second type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T1 AsT1 { get; private set; }
-
-    public Either(T0 asT0 = default, T1 asT1 = default) =>
-        (AsT0, AsT1) =
-        (asT0, asT1);
-
-    public override T As<T>()
+    [Serializable]
+    public class Either<T0, T1> : Either
     {
-        if (AsT0 is T t0)
+        public override int TypeCount => 2;
+        public override string[] TypeNames => new[] { typeof(T0).Name, typeof(T1).Name };
+
+        /// <summary>
+        /// The stored value of the first type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T0 AsT0 { get; private set; }
+
+        /// <summary>
+        /// The stored value of the second type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T1 AsT1 { get; private set; }
+
+        public Either(T0 asT0 = default, T1 asT1 = default) =>
+            (AsT0, AsT1) =
+            (asT0, asT1);
+
+        public override T As<T>()
         {
-            Index = 0;
-            return t0;
+            if (AsT0 is T t0)
+            {
+                Index = 0;
+                return t0;
+            }
+
+            if (AsT1 is T t1)
+            {
+                Index = 1;
+                return t1;
+            }
+
+            return default;
         }
 
-        if (AsT1 is T t1)
+        /// <summary>
+        /// Converts the stored value of the currently selected type to a value of type T.
+        /// </summary>
+        /// <param name="ifT0">If the first type is selected, converts that value to type T.</param>
+        /// <param name="ifT1">If the second type is selected, converts that value to type T.</param>
+        /// <typeparam name="T">The type to convert to.</typeparam>
+        /// <returns>The stored value of the currently selected type converted to type T.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public T Match<T>(Func<T0, T> ifT0, Func<T1, T> ifT1)
         {
-            Index = 1;
-            return t1;
+            return Index switch
+            {
+                0 => ifT0(AsT0),
+                1 => ifT1(AsT1),
+                _ => throw new ArgumentOutOfRangeException(
+                    $"Index {Index} is out of range; this Either only has {TypeCount} types.")
+            };
         }
-
-        return default;
     }
 
-    /// <summary>
-    /// Converts the stored value of the currently selected type to a value of type T.
-    /// </summary>
-    /// <param name="ifT0">If the first type is selected, converts that value to type T.</param>
-    /// <param name="ifT1">If the second type is selected, converts that value to type T.</param>
-    /// <typeparam name="T">The type to convert to.</typeparam>
-    /// <returns>The stored value of the currently selected type converted to type T.</returns>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public T Match<T>(Func<T0, T> ifT0, Func<T1, T> ifT1)
+    [Serializable]
+    public class Either<T0, T1, T2> : Either
     {
-        return Index switch
+        public override int TypeCount => 3;
+        public override string[] TypeNames => new[] { typeof(T0).Name, typeof(T1).Name, typeof(T2).Name };
+
+        /// <summary>
+        /// The stored value of the first type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T0 AsT0 { get; private set; }
+
+        /// <summary>
+        /// The stored value of the second type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T1 AsT1 { get; private set; }
+
+        /// <summary>
+        /// The stored value of the third type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T2 AsT2 { get; private set; }
+
+        public Either(T0 asT0 = default, T1 asT1 = default, T2 asT2 = default) =>
+            (AsT0, AsT1, AsT2) =
+            (asT0, asT1, asT2);
+
+        public override T As<T>()
         {
-            0 => ifT0(AsT0),
-            1 => ifT1(AsT1),
-            _ => throw new ArgumentOutOfRangeException(
-                $"Index {Index} is out of range; this Either only has {TypeCount} types.")
-        };
-    }
-}
+            if (AsT0 is T t0)
+            {
+                Index = 0;
+                return t0;
+            }
 
-[Serializable]
-public class Either<T0, T1, T2> : Either
-{
-    public override int TypeCount => 3;
-    public override string[] TypeNames => new[] { typeof(T0).Name, typeof(T1).Name, typeof(T2).Name };
+            if (AsT1 is T t1)
+            {
+                Index = 1;
+                return t1;
+            }
 
-    /// <summary>
-    /// The stored value of the first type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T0 AsT0 { get; private set; }
+            if (AsT2 is T t2)
+            {
+                Index = 2;
+                return t2;
+            }
 
-    /// <summary>
-    /// The stored value of the second type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T1 AsT1 { get; private set; }
-
-    /// <summary>
-    /// The stored value of the third type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T2 AsT2 { get; private set; }
-
-    public Either(T0 asT0 = default, T1 asT1 = default, T2 asT2 = default) =>
-        (AsT0, AsT1, AsT2) =
-        (asT0, asT1, asT2);
-
-    public override T As<T>()
-    {
-        if (AsT0 is T t0)
-        {
-            Index = 0;
-            return t0;
+            return default;
         }
 
-        if (AsT1 is T t1)
+        /// <summary>
+        /// Converts the stored value of the currently selected type to a value of type T.
+        /// </summary>
+        /// <param name="ifT0">If the first type is selected, converts that value to type T.</param>
+        /// <param name="ifT1">If the second type is selected, converts that value to type T.</param>
+        /// <param name="ifT2">If the third type is selected, converts that value to type T.</param>
+        /// <typeparam name="T">The type to convert to.</typeparam>
+        /// <returns>The stored value of the currently selected type converted to type T.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public T Match<T>(Func<T0, T> ifT0, Func<T1, T> ifT1, Func<T2, T> ifT2)
         {
-            Index = 1;
-            return t1;
+            return Index switch
+            {
+                0 => ifT0(AsT0),
+                1 => ifT1(AsT1),
+                2 => ifT2(AsT2),
+                _ => throw new ArgumentOutOfRangeException(
+                    $"Index {Index} is out of range; this Either only has {TypeCount} types.")
+            };
         }
-
-        if (AsT2 is T t2)
-        {
-            Index = 2;
-            return t2;
-        }
-
-        return default;
-    }
-
-    /// <summary>
-    /// Converts the stored value of the currently selected type to a value of type T.
-    /// </summary>
-    /// <param name="ifT0">If the first type is selected, converts that value to type T.</param>
-    /// <param name="ifT1">If the second type is selected, converts that value to type T.</param>
-    /// <param name="ifT2">If the third type is selected, converts that value to type T.</param>
-    /// <typeparam name="T">The type to convert to.</typeparam>
-    /// <returns>The stored value of the currently selected type converted to type T.</returns>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public T Match<T>(Func<T0, T> ifT0, Func<T1, T> ifT1, Func<T2, T> ifT2)
-    {
-        return Index switch
-        {
-            0 => ifT0(AsT0),
-            1 => ifT1(AsT1),
-            2 => ifT2(AsT2),
-            _ => throw new ArgumentOutOfRangeException(
-                $"Index {Index} is out of range; this Either only has {TypeCount} types.")
-        };
-    }
-}
-
-[Serializable]
-public class Either<T0, T1, T2, T3> : Either
-{
-    public override int TypeCount => 4;
-    public override string[] TypeNames => new[] { typeof(T0).Name, typeof(T1).Name, typeof(T2).Name, typeof(T3).Name };
-
-    /// <summary>
-    /// The stored value of the first type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T0 AsT0 { get; private set; }
-
-    /// <summary>
-    /// The stored value of the second type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T1 AsT1 { get; private set; }
-
-    /// <summary>
-    /// The stored value of the third type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T2 AsT2 { get; private set; }
-
-    /// <summary>
-    /// The stored value of the fourth type in this object's generic type signature.
-    /// </summary>
-    [field: SerializeField]
-    public T3 AsT3 { get; private set; }
-
-    public Either(T0 asT0 = default, T1 asT1 = default, T2 asT2 = default, T3 asT3 = default) =>
-        (AsT0, AsT1, AsT2, AsT3) =
-        (asT0, asT1, asT2, asT3);
-
-    public override T As<T>()
-    {
-        if (AsT0 is T t0)
-        {
-            Index = 0;
-            return t0;
-        }
-
-        if (AsT1 is T t1)
-        {
-            Index = 1;
-            return t1;
-        }
-
-        if (AsT2 is T t2)
-        {
-            Index = 2;
-            return t2;
-        }
-
-        if (AsT3 is T t3)
-        {
-            Index = 3;
-            return t3;
-        }
-
-        return default;
     }
 
-    /// <summary>
-    /// Converts the stored value of the currently selected type to a value of type T.
-    /// </summary>
-    /// <param name="ifT0">If the first type is selected, converts that value to type T.</param>
-    /// <param name="ifT1">If the second type is selected, converts that value to type T.</param>
-    /// <param name="ifT2">If the third type is selected, converts that value to type T.</param>
-    /// <param name="ifT3">If the fourth type is selected, converts that value to type T.</param>
-    /// <typeparam name="T">The type to convert to.</typeparam>
-    /// <returns>The stored value of the currently selected type converted to type T.</returns>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public T Match<T>(Func<T0, T> ifT0, Func<T1, T> ifT1, Func<T2, T> ifT2, Func<T3, T> ifT3)
+    [Serializable]
+    public class Either<T0, T1, T2, T3> : Either
     {
-        return Index switch
+        public override int TypeCount => 4;
+
+        public override string[] TypeNames =>
+            new[] { typeof(T0).Name, typeof(T1).Name, typeof(T2).Name, typeof(T3).Name };
+
+        /// <summary>
+        /// The stored value of the first type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T0 AsT0 { get; private set; }
+
+        /// <summary>
+        /// The stored value of the second type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T1 AsT1 { get; private set; }
+
+        /// <summary>
+        /// The stored value of the third type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T2 AsT2 { get; private set; }
+
+        /// <summary>
+        /// The stored value of the fourth type in this object's generic type signature.
+        /// </summary>
+        [field: SerializeField]
+        public T3 AsT3 { get; private set; }
+
+        public Either(T0 asT0 = default, T1 asT1 = default, T2 asT2 = default, T3 asT3 = default) =>
+            (AsT0, AsT1, AsT2, AsT3) =
+            (asT0, asT1, asT2, asT3);
+
+        public override T As<T>()
         {
-            0 => ifT0(AsT0),
-            1 => ifT1(AsT1),
-            2 => ifT2(AsT2),
-            3 => ifT3(AsT3),
-            _ => throw new ArgumentOutOfRangeException(
-                $"Index {Index} is out of range; this Either only has {TypeCount} types.")
-        };
+            if (AsT0 is T t0)
+            {
+                Index = 0;
+                return t0;
+            }
+
+            if (AsT1 is T t1)
+            {
+                Index = 1;
+                return t1;
+            }
+
+            if (AsT2 is T t2)
+            {
+                Index = 2;
+                return t2;
+            }
+
+            if (AsT3 is T t3)
+            {
+                Index = 3;
+                return t3;
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        /// Converts the stored value of the currently selected type to a value of type T.
+        /// </summary>
+        /// <param name="ifT0">If the first type is selected, converts that value to type T.</param>
+        /// <param name="ifT1">If the second type is selected, converts that value to type T.</param>
+        /// <param name="ifT2">If the third type is selected, converts that value to type T.</param>
+        /// <param name="ifT3">If the fourth type is selected, converts that value to type T.</param>
+        /// <typeparam name="T">The type to convert to.</typeparam>
+        /// <returns>The stored value of the currently selected type converted to type T.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public T Match<T>(Func<T0, T> ifT0, Func<T1, T> ifT1, Func<T2, T> ifT2, Func<T3, T> ifT3)
+        {
+            return Index switch
+            {
+                0 => ifT0(AsT0),
+                1 => ifT1(AsT1),
+                2 => ifT2(AsT2),
+                3 => ifT3(AsT3),
+                _ => throw new ArgumentOutOfRangeException(
+                    $"Index {Index} is out of range; this Either only has {TypeCount} types.")
+            };
+        }
     }
 }
