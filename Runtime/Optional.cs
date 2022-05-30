834cadfb18b4fa2b9b2dc641193bb473d46767e2
bgsulz <bgs.mail1234@gmail.com>
2022-05-30 16:23:23 -0400

    Added Optional utility type

---
+++ Runtime/Optional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Extra.Either
+{
+    [Serializable]
+    public struct None
+    {
+        public static None Instance = new();
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(None))]
+    public class NoneDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+            => EditorGUI.LabelField(position, "None");
+    }
+#endif
+
+    [Serializable]
+    public class Optional<T> : Either<T, None> { }
+}
