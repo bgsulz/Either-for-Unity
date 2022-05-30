3afa6babbae7e93dbe36d2145569f7d9b8bb5bd2
bgsulz <bgs.mail1234@gmail.com>
2022-05-30 17:21:51 -0400

    Optional<T> operators for implicit type conversion

--- Runtime/Optional.cs
+++ Runtime/Optional.cs
@@ -7,7 +7,7 @@ namespace Extra.Either
     [Serializable]
     public struct None
     {
-        public static None Instance = new();
+        public static None Instance = default;
     }
 
 #if UNITY_EDITOR
@@ -20,5 +20,14 @@ namespace Extra.Either
 #endif
 
     [Serializable]
-    public class Optional<T> : Either<T, None> { }
+    public class Optional<T> : Either<T, None>
+    {
+        public Optional(int index = 0, T asT = default, None asNone = default) : base(index, asT, asNone) { }
+
+        public static implicit operator T(Optional<T> optional) => optional.AsT0;
+        public static implicit operator None(Optional<T> optional) => optional.AsT1;
+
+        public static implicit operator Optional<T>(T value) => new(0, asT: value);
+        public static implicit operator Optional<T>(None value) => new(1);
+    }
 }
